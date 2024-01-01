0e0ddf3319592c09d63366464679cec489603958
Aditi Potdar <aditipotdar22@gmail.com>
2024-01-02 01:48:21 +0530

    removed api token

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,6 +6,5 @@ import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-const apitoken = process.env.NEXT_PUBLIC_SANITY_API_TOKEN
 
-export default defineCliConfig({ api: { projectId, dataset, apitoken } })
+export default defineCliConfig({ api: { projectId, dataset} })
